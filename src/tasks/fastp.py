2271d75710febde4431bb49b23e60b9937d54601
pryce-turner <pryce.turner@gmail.com>
2023-12-08 16:11:31 -0800

    Finished make_all for filt samples and tests

--- src/tasks/fastp.py
+++ src/tasks/fastp.py
@@ -26,10 +26,11 @@ def pyfastp(rs: RawSample) -> FiltSample:
         FiltSample: A FiltSample object representing the filtered and preprocessed data.
     """
     ldir = Path(current_context().working_directory)
-    o1p = ldir.joinpath(f"{rs.sample}_1_filt.fq.gz")
-    o2p = ldir.joinpath(f"{rs.sample}_2_filt.fq.gz")
-    rep = ldir.joinpath(f"{rs.sample}_report.json")
-    logger.debug(f"Writing filtered reads to {o1p} and {o2p} and report to {rep}")
+    o1, o2, rep = FiltSample(rs.sample).make_filenames()
+    o1p = ldir.joinpath(o1)
+    o2p = ldir.joinpath(o2)
+    repp = ldir.joinpath(rep)
+    logger.debug(f"Writing filtered reads to {o1p} and {o2p} and report to {repp}")
 
     cmd = ["fastp", "-i", rs.raw_r1, "-I", rs.raw_r2, "-o", o1p, "-O", o2p, "-j", rep]
     logger.debug(f"Running command: {cmd}")
