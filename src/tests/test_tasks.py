36933bf67742cb1b2924bfacd353700b7449c222
pryce-turner <pryce.turner@gmail.com>
2023-12-21 16:43:19 -0800

    Cleaned up some paths

--- src/tests/test_tasks.py
+++ src/tests/test_tasks.py
@@ -5,15 +5,15 @@ from tasks.utils import prepare_raw_samples
 from tasks.sample_types import RawSample, FiltSample, SamFile
 from tasks.fastp import pyfastp
 from tasks.fastqc import fastqc
-from config import test_assets
+from config import test_assets, logger
 
-# def test_fastqc():
-#     qc_samp = fastqc(seq_dir=test_assets['local_seq_dir'])
-#     assert isinstance(qc_samp, FlyteDirectory)
+def test_fastqc():
+    qc_samp = fastqc(seq_dir=test_assets['local_tiny_seq_dir'])
+    logger.debug(f'fastqc returned {qc_samp}')
+    assert isinstance(qc_samp, FlyteDirectory)
 
 def test_fastp():
     raw_samp = RawSample.make_all(Path(test_assets['local_seq_dir']))[0]
-    print(raw_samp)
-    # filt_samp = pyfastp(rs=raw_samp)
-    # assert isinstance(filt_samp, FiltSample)
+    filt_samp = pyfastp(rs=raw_samp)
+    assert isinstance(filt_samp, FiltSample)
 
