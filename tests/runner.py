3fabbb9faa8df79ee53cc563168c114bbf8cfde5
pryce-turner <pryce.turner@gmail.com>
2024-06-13 15:34:18 -0700

    Initial docker test runner impl
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>

--- tests/runner.py
+++ tests/runner.py
@@ -5,41 +5,40 @@ from config import proj_rt, main_img_test_fqn
 def run_pytest_in_docker():
     client = docker.from_env()
     
-    # try:
-    # Run the Docker container
-    print("\nRunning pytest in Docker container...")
-    container = client.containers.run(
-        image=main_img_test_fqn,
-        volumes={
-            proj_rt.joinpath('tests'): {
-                'bind': '/root/tests',
-                'mode': 'rw',
+    try:
+        # Run the Docker container
+        print("\nRunning pytest in Docker container...")
+        container = client.containers.run(
+            image=main_img_test_fqn,
+            volumes={
+                proj_rt.joinpath('tests'): {
+                    'bind': '/root/tests',
+                    'mode': 'rw',
+                },
+                # Mount unionbio so it's discoverable in PYTHONPATH
+                proj_rt.joinpath("src/unionbio"): {
+                    'bind': '/root/unionbio',
+                    'mode': 'rw',
+                }
             },
-            # Mount unionbio so it's discoverable in PYTHONPATH
-            proj_rt.joinpath("src/unionbio"): {
-                'bind': '/root/unionbio',
-                'mode': 'rw',
-            }
-        },
-        # working_dir="/app",
-        command="pytest /root/tests/test_sample_types.py",
-        # command='python -c "import unionbio"',
-        remove=True,
-        stdout=True,
-        stderr=True
-    )
-    
-    # Print the output of the pytest command
-    print(container)
-    # except docker.errors.ContainerError as e:
-    #     # Handle errors that occur during container run
-    #     print(f"Container error: {e}")
-    #     print(e.stderr.decode())
-    # except docker.errors.ImageNotFound as e:
-    #     # Handle errors if the image is not found
-    #     print(f"Image not found: {e}")
-    # except docker.errors.APIError as e:
-    #     # Handle general API errors
-    #     print(f"API error: {e}")
+            # working_dir="/app",
+            command="pytest /root/tests/",
+            # remove=True,
+            # stdout=True,
+            # stderr=True
+        )
+        # Print the output of the pytest command
+        print(container)
+    except docker.errors.ContainerError as e:
+        # Handle errors that occur during container run
+        print(f"Container error: {e}")
+        print(e.container.logs().decode('utf-8'))
+        # print(e.stderr.decode())
+    except docker.errors.ImageNotFound as e:
+        # Handle errors if the image is not found
+        print(f"Image not found: {e}")
+    except docker.errors.APIError as e:
+        # Handle general API errors
+        print(f"API error: {e}")
 
 run_pytest_in_docker()
