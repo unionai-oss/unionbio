36c96d089ea74e45b3f99730cf9b6b137e4ce996
pryce-turner <pryce.turner@gmail.com>
2024-06-13 16:07:28 -0700

    Improved error handling
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>

--- tests/runner.py
+++ tests/runner.py
@@ -8,7 +8,9 @@ def run_pytest_in_docker():
     try:
         # Run the Docker container
         print("\nRunning pytest in Docker container...")
+        con_name = "unionbio-main-test-container"
         container = client.containers.run(
+            name=con_name,
             image=main_img_test_fqn,
             volumes={
                 proj_rt.joinpath('tests'): {
@@ -22,23 +24,28 @@ def run_pytest_in_docker():
                 }
             },
             # working_dir="/app",
-            command="pytest /root/tests/",
+            command="pytest /root/tests/main/",
             # remove=True,
-            # stdout=True,
-            # stderr=True
+            stdout=True,
+            stderr=True
         )
         # Print the output of the pytest command
-        print(container)
+        print(container.decode('utf-8'))
     except docker.errors.ContainerError as e:
         # Handle errors that occur during container run
         print(f"Container error: {e}")
         print(e.container.logs().decode('utf-8'))
-        # print(e.stderr.decode())
     except docker.errors.ImageNotFound as e:
         # Handle errors if the image is not found
         print(f"Image not found: {e}")
     except docker.errors.APIError as e:
         # Handle general API errors
         print(f"API error: {e}")
+    finally:
+        # Ensure the container is cleaned up
+        con = client.containers.get(con_name)
+        if con is not None:
+            con.remove(force=True)
+            print(f"Container {con.id} removed")
 
 run_pytest_in_docker()
