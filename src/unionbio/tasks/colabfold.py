39a54ba603f1967adbd1e5bcf2bdbbb40abe3ed4
pryce-turner <pryce.turner@gmail.com>
2024-09-25 16:21:21 -0700

    Added generate MSAs and predict structure colabfold tasks
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>

--- src/unionbio/tasks/colabfold.py
+++ src/unionbio/tasks/colabfold.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 from pathlib import Path
+from typing import Tuple, List
 from flytekit import task, workflow, Resources, current_context
 from flytekit.types.file import FlyteFile
 from flytekit.types.directory import FlyteDirectory
@@ -13,6 +14,7 @@ from unionbio.types.protein import Protein
 
 DB_LOC = "/root/af_dbs/"
 CPU = "15"
+os.environ["ALPHAFOLD_DATA_DIR"] = DB_LOC
 
 
 actor = ActorEnvironment(
@@ -26,6 +28,7 @@ actor = ActorEnvironment(
     ),
     container_image=colabfold_img_fqn,
 )
+
 
 @actor.task
 def dl_dbs(
@@ -74,25 +77,47 @@ def dl_dbs(
     logger.debug(f"Database files: {os.listdir(output_loc)}")
     return output_loc
 
+
 @actor.task
-def cf_search(seq: FlyteFile, db_loc: str = DB_LOC) -> FlyteDirectory:
+def generate_msas(
+    seq: FlyteFile, db_loc: str = DB_LOC
+) -> tuple[FlyteDirectory, list[str], dict[str, str]]:
+    from colabfold.batch import get_msa_and_templates
+
     outdir = Path(current_context.working_directory).joinpath("search_out")
-    search_cmd = [
-        "colabfold_search",
-        "--use-env 1",
-        "--use-templates 1",
-        "--db-load-mode 2",
-        "--db2 pdb100_230517",
-        "--mmseqs /root/micromamba/envs/dev/bin/mmseqs",
-        f"--threads {CPU}",
-        seq.path,
-        db_loc,
-        outdir,
-    ]
-    cmd_str = " ".join(search_cmd)
-    logger.info(f"Running search with command: {cmd_str}")
     start = time.time()
-    subproc_execute(command=search_cmd)
+    a3m_files, template_results = get_msa_and_templates(
+        seq.path,
+        msa_mode="mmseqs2_uniref_env",
+        use_templates=True,
+        msa_output_dir=outdir,
+    )
     logger.info(f"Search completed in {time.time() - start} seconds")
     logger.debug(f"Search output: {os.listdir(outdir)}")
-    return outdir
+    return FlyteDirectory(outdir), a3m_files, template_results
+
+
+@actor.task
+def predict_structure(
+    seq: FlyteFile,
+    msa: FlyteDirectory,
+    a3m_files: list[str],
+    templates: dict[str, str],
+) -> FlyteDirectory:
+    from colabfold.predict import run_alphafold
+
+    # Check if MSA files are present e.g. Actor mode
+    if not os.listdir(msa.path):
+        msa.download()
+
+    results = Path(current_context.working_directory).joinpath("af_results")
+    start = time.time()
+    prediction_results = run_alphafold(
+        fasta_path=seq.path,
+        a3m_files=a3m_files,
+        templates_results=templates,
+        result_dir=results,
+        model_type="alphafold2_multimer_v3",
+    )
+    logger.info(f"Prediction completed in {time.time() - start} seconds")
+    return FlyteDirectory(path=results)
