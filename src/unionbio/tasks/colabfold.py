c91f43f909dc8ca5433ca54a8099fde3efc4cc6d
Pryce Turner <pryce.turner@gmail.com>
2024-10-29 22:52:55 +0000

    Tidied up some type bugs, working task flow

--- src/unionbio/tasks/colabfold.py
+++ src/unionbio/tasks/colabfold.py
@@ -8,7 +8,7 @@ from flytekit.types.directory import FlyteDirectory
 from flytekit.extras.tasks.shell import subproc_execute
 from union.actor import ActorEnvironment
 
-sys.path = ['/home/flytekit/workspace/unionbio/src', '/root/micromamba/envs/dev/bin', '/root/unionbio/src/unionbio', '/root', '/root/micromamba/envs/dev/lib/python310.zip', '/root/micromamba/envs/dev/lib/python3.10', '/root/micromamba/envs/dev/lib/python3.10/lib-dynload', '/root/micromamba/envs/dev/lib/python3.10/site-packages']
+# sys.path = ['/home/flytekit/workspace/unionbio/src', '/root/micromamba/envs/dev/bin', '/root/unionbio/src/unionbio', '/root', '/root/micromamba/envs/dev/lib/python310.zip', '/root/micromamba/envs/dev/lib/python3.10', '/root/micromamba/envs/dev/lib/python3.10/lib-dynload', '/root/micromamba/envs/dev/lib/python3.10/site-packages']
 
 from unionbio.config import colabfold_img_fqn, logger
 
@@ -90,12 +90,13 @@ def s3_sync(
     logger.info(f"Downloading databases with command: {cmd_str}")
     start = time.time()
 
-    try:
-        subproc_execute(command=cmd_str, shell=True)
-    except RuntimeError:
-        retries -= 1
-        if retries = 0: raise
-        continue
+    while retries > 0:
+        try:
+            subproc_execute(command=cmd_str, shell=True)
+        except RuntimeError:
+            retries -= 1
+            if retries == 0: raise
+            continue
     
     elapsed = time.time() - start
     logger.info(f"Downloaded in {elapsed} seconds ({elapsed/3600} hours)")
@@ -107,16 +108,17 @@ def s3_sync(
 def cf_search(
     seq: FlyteFile,
     db_path: str = DB_LOC,
+    outdir: str = None,
 ) -> tuple[FlyteFile, FlyteFile]:
 
-    indir = Path(current_context().working_directory).joinpath("inputs")
-    outdir = Path(current_context().working_directory).joinpath("outputs")
-    os.makedirs(indir, exist_ok=True)
+    outdir = outdir or str(Path(current_context().working_directory).joinpath("outputs"))
     seq.download()
 
     t = time.time()
     cmd = [
         "colabfold_search",
+        "-s",
+        "1",
         "--use-env",
         "1",
         "--use-templates",
@@ -129,29 +131,36 @@ def cf_search(
         CPU,
         seq.path,
         db_path,
-        str(outdir),
+        outdir,
     ]
     logger.debug(f"Running MMSeqs search on {seq.path} with command:")
     logger.debug(' '.join(cmd))
-    subproc_execute(cmd)
+    # subproc_execute(cmd)
+    logger.info(f"Created the following outputs in {time.time() - t} seconds:")
+    logger.info(f"MSA files in {Path(outdir).absolute()}: {os.listdir(outdir)}")
 
     for fn in os.listdir(outdir):
         path = Path(fn).absolute()
         if path.suffix == ".m8":
-            hitfile = FlyteFile(path=path)
+            hitfile = FlyteFile(path=str(path))
         if path.suffix == ".a3m":
-            msa = FlyteFile(path=path)
+            msa = FlyteFile(path=str(path))
 
-    logger.info(f"Created the following outputs in {time.time() - t} seconds:")
-    logger.info(f"MSA files in {Path(outdir).absolute()}: {os.listdir(outdir)}")
+    logger.debug(f"Returning {hitfile} and {msa}")
 
     return hitfile, msa
 
 
 @task
-def af_predict(hitfile: FlyteFile, msa: FlyteFile, mmcif_loc: str) -> FlyteDirectory:
+def af_predict(
+    hitfile: FlyteFile, 
+    msa: FlyteFile, 
+    mmcif_loc: str = None,
+    outdir: str = None,
+    ) -> FlyteDirectory:
 
-    outdir = Path(current_context().working_directory).joinpath("outputs")
+    outdir = outdir or str(Path(current_context().working_directory).joinpath("outputs"))
+    mmcif_loc = mmcif_loc or str(Path(DB_LOC).joinpath("pdb_mmcif", "mmcif_files"))
     msa.download()
     hitfile.download()
     logger.info(f"Running AlphaFold on {msa.path} and {hitfile.path}")
@@ -168,7 +177,7 @@ def af_predict(hitfile: FlyteFile, msa: FlyteFile, mmcif_loc: str) -> FlyteDirec
         "--random-seed",
         "0",
         msa.path,
-        str(outdir),
+        outdir,
     ]
     logger.debug(f"Executing:")
     logger.debug(' '.join(cmd))
@@ -177,8 +186,12 @@ def af_predict(hitfile: FlyteFile, msa: FlyteFile, mmcif_loc: str) -> FlyteDirec
     return FlyteDirectory(path=outdir)
 
 @workflow
-def cf_wf() -> FlyteDirectory:
-    dl = s3_sync(db_uri="s3://pryce-test/colabfold/")
-    hitfile, msa = cf_search(seq="/mnt/folding_io/fastas/P01308.fasta")
-    af = af_predict(hitfile=hitfile, msa=msa, mmcif_loc="/home/ubuntu/pdb_mmcif/mmcif_files")
-    return af
+def cf_wf():
+    # dl = s3_sync(db_uri="s3://pryce-test/colabfold/")
+    hitfile, msa = cf_search(seq="/mnt/folding_io/fastas/P01308.fasta", outdir="/tmp/flyte20mwg4m3/user_spacen0xnt8re/outputs")
+    af = af_predict(
+        hitfile="/tmp/flyteiwbp4nxz/user_spaceyifdbswm/outputs/sp_P01308_INS_HUMAN_Insulin_OS_Homo_sapiens_OX_9606_GN_INS_PE_1_SV_1_pdb100_230517.m8",
+        msa="/tmp/flyteiwbp4nxz/user_spaceyifdbswm/outputs/sp_P01308_INS_HUMAN_Insulin_OS_Homo_sapiens_OX_9606_GN_INS_PE_1_SV_1.a3m",
+        mmcif_loc="/mnt/pdb_mmcif/mmcif_files"
+        )
+    # return af
