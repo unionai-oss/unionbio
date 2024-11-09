5be463bc02dd050bc746f3f19c3597f1722ad199
pryce-turner <pryce.turner@gmail.com>
2024-11-08 17:07:49 -0800

    Installed mmseqs via latest
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>

--- images.py
+++ images.py
@@ -73,7 +73,6 @@ colabfold_img = ImageSpec(
     source_root=prod_rt,
     conda_channels=["bioconda", "conda-forge"],
     conda_packages=[
-        "mmseqs2", 
         "colabfold[alphafold]", 
         "jax[cuda12]", 
         "tensorflow"
@@ -81,6 +80,8 @@ colabfold_img = ImageSpec(
     commands=[
         # Install gcloud
         'echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && apt-get update -y && apt-get install google-cloud-cli -y',
+        # Install mmseqs
+        'wget https://mmseqs.com/latest/mmseqs-linux-avx2.tar.gz; tar xvfz mmseqs-linux-avx2.tar.gz; export PATH=$(pwd)/mmseqs/bin/:$PATH',
         ],
     registry=current_registry,
 )
